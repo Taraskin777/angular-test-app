134872b78e1ec93e78f3f6f59a8e8099329f79bb
Taras Noga <taraskin2012@gmail.com>
2024-01-03 19:38:48 +0200

    add onInputChange and checkPasswordStrength methods, add conditions to app.componnent.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,40 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'My Test App';
+  password: string = '';
+  passwordStrength: string = '';
+  ngOnInit(): void {
+    this.checkPasswordStrength();
+  }
+  onInputChange(event: Event): void {
+    const passwordValue = (event.target as HTMLInputElement).value;
+    this.password = passwordValue;
+    this.checkPasswordStrength();
+  }
+  checkPasswordStrength(): void {
+    const passwordLength = this.password.length;
+    const onlyLetters = /^[a-zA-Z]+$/.test(this.password);
+    const onlyNumbers = /^[0-9]+$/.test(this.password);
+    const onlySymbols = /^[^a-zA-Z0-9]+$/.test(this.password);
+
+    if (passwordLength === 0) {
+      this.passwordStrength = 'Empty';
+    } else if (passwordLength < 8) {
+      this.passwordStrength = 'Weak';
+    } else if (onlyLetters || onlyNumbers || onlySymbols) {
+      this.passwordStrength = 'Easy';
+    } else if (
+      /^[a-zA-Z]+[0-9]+$/.test(this.password) ||
+      /^[a-zA-Z]+[^a-zA-Z0-9]+$/.test(this.password) ||
+      /^[0-9]+[^a-zA-Z0-9]+$/.test(this.password)
+    ) {
+      this.passwordStrength = 'Medium';
+    } else {
+      this.passwordStrength = 'Strong';
+    }
+  }
 }
